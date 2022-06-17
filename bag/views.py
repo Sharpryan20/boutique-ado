5d92e8d1ded47eb1e5d2886f88a20fb405d7f6cf
Sharpryan20 <sharpryan01@icloud.com>
2022-06-17 19:23:13 +0000

    added checkout views and templates

--- bag/views.py
+++ bag/views.py
@@ -101,4 +101,5 @@ def remove_from_bag(request, item_id):
         request.session['bag'] = bag
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request, f'Error removing item: ')
         return HttpResponse(status=500)
