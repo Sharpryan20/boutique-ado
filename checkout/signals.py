2399d3bc3666935e95e7a5adf5db5a814cbd9cd0
Sharpryan20 <sharpryan01@icloud.com>
2022-06-21 16:41:52 +0000

    added loading overlay

--- checkout/signals.py
+++ checkout/signals.py
@@ -8,11 +8,11 @@ def update_on_save(sender, instance, created, **kwargs):
     """
     Update order total on lineitem update/create
     """
-    instance.order.update.total()
+    instance.order.update_total()
 
-@receiver(post_save, sender=OrderLineItem)
-def update_on_save(sender, instance, **kwargs):
+@receiver(post_delete, sender=OrderLineItem)
+def update_on_delete(sender, instance, **kwargs):
     """
     Update order total on lineitem delete
     """
-    instance.order.update.total()
+    instance.order.update_total()
